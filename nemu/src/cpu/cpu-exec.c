0b5e465db9685eb0e45a0cfbe07cd59136ce0412
tracer-ysyx <tracer@ysyx.org>
2025-02-18 20:35:38 +0800

    > compile NEMU ysyx_22040000 张三 Linux dengzibin-NBM-WXX9 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:35:38 up 6 days,  2:34,  1 user,  load average: 0.18, 0.42, 0.42

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -41,7 +41,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
-  s->pc = pc;
+  s->pc = pc;//pc=uint_64
   s->snpc = pc;
   isa_exec_once(s);
   cpu.pc = s->dnpc;
