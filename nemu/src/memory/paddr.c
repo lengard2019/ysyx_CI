cbe6f0e9b817cdcc1bc1bd2088698dc297b16e13
tracer-ysyx <tracer@ysyx.org>
2025-02-19 11:18:27 +0800

    > compile NEMU ysyx_22040000 张三 Linux dengzibin-NBM-WXX9 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:18:27 up 6 days, 17:17,  1 user,  load average: 0.63, 0.45, 0.39

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -51,6 +51,7 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
+  printf("mark\n");
   if (likely(in_pmem(addr))) return pmem_read(addr, len);//likely大概率发生
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
