c7172c7a2d405250538aa3b154a92ca681600e9f
tracer-ysyx <tracer@ysyx.org>
2025-02-24 12:34:18 +0800

    > compile NEMU ysyx_22040000 张三 Linux dengzibin-NBM-WXX9 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:34:18 up 11 days, 18:32,  1 user,  load average: 0.78, 0.70, 0.56

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -65,6 +65,8 @@ static int cmd_info(char *args);
 
 static int cmd_x(char *args);
 
+static int cmd_p(char *args);
+
 static struct {
   const char *name;
   const char *description;
@@ -73,9 +75,10 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-  { "si", "execute N steps of the program", cmd_si},
-  { "info", "print the situation of the program, r/w", cmd_info},
-  { "x", "scan ddr", cmd_x},
+  { "si", "Execute N steps of the program", cmd_si},
+  { "info", "Print the situation of the program, r/w", cmd_info},
+  { "x", "Scan ddr", cmd_x},
+  { "p", "Mathematical expression evaluation", cmd_p}
 
 
   /* TODO: Add more commands */
@@ -84,13 +87,20 @@ static struct {
 
 #define NR_CMD ARRLEN(cmd_table)
 
+
+
 static int cmd_si(char *args){
-    int step = 0;
-    if(args == NULL)
-        step = 1;
-    else
-        sscanf(args,"%d",&step);// 读入 Step
-    cpu_exec(step);
+  int step = 0;
+  if(args == NULL)
+    step = 1;
+  else
+    sscanf(args,"%d",&step);// 读入 Step
+  cpu_exec(step);
+  return 0;
+}
+
+static int cmd_p(char *args){
+    printf("%s",args);
     return 0;
 }
 
@@ -191,4 +201,8 @@ void init_sdb() {
 
   /* Initialize the watchpoint pool. */
   init_wp_pool();
+}
+
+void make_token(){
+
 }
