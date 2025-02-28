edfce8f231218ef2adaf520f2526035ab62b503d
tracer-ysyx <tracer@ysyx.org>
2025-02-28 15:54:10 +0800

    > compile NEMU ysyx_22040000 张三 Linux dengzibin-NBM-WXX9 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:54:10 up 15 days, 21:52,  1 user,  load average: 0.43, 0.63, 0.67

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -207,6 +207,7 @@ static bool check_parentheses(int p, int q)//判断总式或子式是否符合�
 uint32_t eval(int p, int q) {
   if (p > q) {
     /* Bad expression */
+    printf("%d %d", p, q);
     assert(0);
     return -1;
   }
