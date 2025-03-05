b8c334719a1c70d1e8c887bf10bbac1c2e9d2b84
tracer-ysyx <tracer@ysyx.org>
2025-03-05 17:39:34 +0800

    > compile NEMU ysyx_22040000 张三 Linux dengzibin-NBM-WXX9 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:39:34 up 20 days, 23:37,  1 user,  load average: 0.25, 0.43, 0.47

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -331,7 +331,7 @@ static void token_special(){
         {
           if(tokens[i].str[j] < '0' || tokens[i].str[j] > '9')
           {
-            printf("the number of position %d is wrong", i);
+            printf("the number of position %d is wrong\n", i);
             assert(0);
           }
         }
