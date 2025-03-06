57f488d2247c5b1623793c689c7f2e002856e3a1
tracer-ysyx <tracer@ysyx.org>
2025-03-06 22:44:49 +0800

    > compile NEMU ysyx_22040000 张三 Linux dengzibin-NBM-WXX9 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:44:49 up 22 days,  4:42,  1 user,  load average: 0.19, 0.25, 0.20

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -354,11 +354,8 @@ static void token_special(){
     {
       if(tokens[i].str[0] == '0' && (tokens[i].str[1] == 'x'|| tokens[i].str[1] == 'X'))// Hex num
       {
-        printf("%s\n",tokens[i].str);
         int value = strtol(tokens[i].str, NULL, 16);
-        printf("%d\n", value);
         int2char(value, tokens[i].str);
-        printf("%s\n",tokens[i].str);
       }
       else
       {
