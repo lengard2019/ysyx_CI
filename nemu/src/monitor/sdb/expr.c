cc53c6e8d49e335681dc4894da0090d4809341db
tracer-ysyx <tracer@ysyx.org>
2025-03-08 14:47:43 +0800

    > compile NEMU ysyx_22040000 张三 Linux dengzibin-NBM-WXX9 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:47:43 up 23 days, 20:45,  1 user,  load average: 0.29, 0.34, 0.44

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -571,6 +571,7 @@ word_t expr(char *e, bool *success) {
 
   // /* TODO: Insert codes to evaluate the expression. */
   result = eval(0, nr_token - 1);
+  *success = true;
   for (int i = 0; i < nr_token; i++){
     tokens[i].type = 0;
     memset(tokens[i].str, '\0', sizeof(tokens[i].str));
