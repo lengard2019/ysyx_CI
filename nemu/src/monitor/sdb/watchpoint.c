363308ed736a2557ae5c0c5e5192b50f7f1463a0
tracer-ysyx <tracer@ysyx.org>
2025-03-18 17:32:49 +0800

    > compile NEMU ysyx_22040000 张三 Linux dengzibin-NBM-WXX9 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:32:49 up 33 days, 23:30,  1 user,  load average: 0.25, 0.44, 0.47

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -120,7 +120,7 @@ bool watchpoint_diff(int i){
   {
     bool success = false;
     int tmp = expr(wp_pool[i].expr,&success);
-    printf("%x\n",tmp);
+    // printf("%x\n",tmp);
     if(success){
       if(tmp != wp_pool[i].old_val)
       {
