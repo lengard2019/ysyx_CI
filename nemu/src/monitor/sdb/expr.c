4c30f718bc44bd5b131d36abea2d3fe6721bafc7
tracer-ysyx <tracer@ysyx.org>
2025-03-24 13:01:51 +0800

    > compile NEMU ysyx_22040000 张三 Linux dengzibin-NBM-WXX9 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:01:51 up  2:28,  1 user,  load average: 0.46, 0.56, 0.60

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -500,7 +500,7 @@ uint32_t eval(int p, int q) {
     int op_type =  tokens[op].type;
     printf("501 %d %d\n",p,op - 1);
     uint32_t val1 = eval(p, op - 1);
-    printf("501 %d %d\n",op + 1,q);
+    printf("503 %d %d\n",op + 1,q);
     uint32_t val2 = eval(op + 1, q);
 
     switch (op_type) {
