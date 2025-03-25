197e64aab9140b9ac7e93495b439b59f8a9d55b1
tracer-ysyx <tracer@ysyx.org>
2025-03-25 21:03:43 +0800

    > compile NEMU ysyx_22040000 张三 Linux dengzibin-NBM-WXX9 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:03:43 up  2:18,  1 user,  load average: 0.65, 0.34, 0.21

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -315,7 +315,8 @@ static void token_special(){
       vaddr_t pc = cpu_state();
       snprintf(tokens[i].str, 11, "0x%08X", pc);
       tokens[i].type = NUM;
-      printf("318 %s\n",tokens[i].str);
+      printf("%s\n",tokens[i].str);
+      // printf("318 %s\n",tokens[i].str);
 	    // bool flag = true;
 	    // int tmp = isa_reg_str2val(tokens[i].str, &flag);
 	    // if(flag){
@@ -362,33 +363,34 @@ static void token_special(){
   /*
    * NUM
   */
-  for(int i = 0 ; i < nr_token ; i ++)
-  {
-    if(tokens[i].type == NUM)
-    {
-      if(tokens[i].str[0] == '0' && (tokens[i].str[1] == 'x'|| tokens[i].str[1] == 'X'))// Hex num
-      {
-        int value = strtol(tokens[i].str, NULL, 16);
-        printf("372 %x\n",value);
-        int2char(value, tokens[i].str);
-        printf("374 %s\n",tokens[i].str);
-      }
-      else
-      {
-        for(int j = 0; j < strlen(tokens[i].str); j++)
-        {
-          // printf("%s\n",tokens[i].str);
-          if(tokens[i].str[j] < '0' || tokens[i].str[j] > '9')
-          {
-            if(tokens[i].str[j] != '-'){
-              printf("the number of position %d is wrong\n", i);
-              assert(0);
-            }           
-          }
-        }
-      }
-    }
-  }
+  // for(int i = 0 ; i < nr_token ; i ++)
+  // {
+  //   if(tokens[i].type == NUM)
+  //   {
+  //     // if(tokens[i].str[0] == '0' && (tokens[i].str[1] == 'x'|| tokens[i].str[1] == 'X'))// Hex num
+  //     // {
+  //     //   int value = strtol(tokens[i].str, NULL, 16);
+
+  //     //   printf("372 %x\n",value);//
+  //     //   int2char(value, tokens[i].str);
+  //     //   printf("374 %s\n",tokens[i].str);
+  //     // }
+  //     // else
+  //     {
+  //       for(int j = 0; j < strlen(tokens[i].str); j++)
+  //       {
+  //         // printf("%s\n",tokens[i].str);
+  //         if(tokens[i].str[j] < '0' || tokens[i].str[j] > '9')
+  //         {
+  //           if(tokens[i].str[j] != '-'){
+  //             printf("the number of position %d is wrong\n", i);
+  //             assert(0);
+  //           }
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
 
   /*
    * 负数
