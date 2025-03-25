4a863e4fc8d3816622e87ef3df7545217e69ac48
tracer-ysyx <tracer@ysyx.org>
2025-03-25 21:04:16 +0800

    > compile NEMU ysyx_22040000 张三 Linux dengzibin-NBM-WXX9 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:04:16 up  2:19,  1 user,  load average: 0.49, 0.33, 0.21

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -315,7 +315,7 @@ static void token_special(){
       vaddr_t pc = cpu_state();
       snprintf(tokens[i].str, 11, "0x%08X", pc);
       tokens[i].type = NUM;
-      printf("%s\n",tokens[i].str);
+      printf("318 %s\n",tokens[i].str);
       // printf("318 %s\n",tokens[i].str);
 	    // bool flag = true;
 	    // int tmp = isa_reg_str2val(tokens[i].str, &flag);
