b0f645f190be420eed09aeffdbe27743fef052ab
tracer-ysyx <tracer@ysyx.org>
2025-03-29 12:34:17 +0800

    > compile NEMU ysyx_22040000 张三 Linux dengzibin-NBM-WXX9 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:34:17 up 20:28,  1 user,  load average: 0.22, 0.28, 0.33

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -667,7 +667,7 @@ static void gen_rand_op()
 void gen_rand_expr() {
   // buf[0] = '(';
   // index_buf ++;
-  switch (choose(10)) {
+  switch (choose(4)) {
     case 0: 
       gen_num(); 
       break;
