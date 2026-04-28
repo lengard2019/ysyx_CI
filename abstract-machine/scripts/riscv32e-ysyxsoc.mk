007d1685d231dc1c9f1bc0cc3aa10c0cf3a2f1eb
dengzibin <1447762756@qq.com>
2026-04-28 20:13:31 +0800

    ysyxsoc

--- abstract-machine/scripts/riscv32e-ysyxsoc.mk
+++ abstract-machine/scripts/riscv32e-ysyxsoc.mk
@@ -1,5 +1,5 @@
 include $(AM_HOME)/scripts/isa/riscv.mk
-include $(AM_HOME)/scripts/platform/ysyxSoC.mk
+include $(AM_HOME)/scripts/platform/ysyxsoc.mk
 CFLAGS  += -DISA_H=\"riscv/riscv.h\"
 COMMON_CFLAGS += -march=rv32e_zicsr -mabi=ilp32e -mlittle-endian # overwrite
 LDFLAGS       += -melf32lriscv                    # overwrite
